78edcd0048981752f442cace273e11d8c1fdfcfd
nampx <nampx@rikkeisoft.com>
2020-04-28 11:40:02 +0700

    add mail function

--- contact-form.php
+++ contact-form.php
@@ -17,8 +17,8 @@ if (isset($_POST['inputName']) && isset($_POST['inputEmail'])&& isset($_POST['in
 
     $mail->From = $_POST['inputEmail'];
     $mail->FromName = $_POST['inputName'];
-    $mail->AddAddress('your@email.here'); //recipient 
-    $mail->Subject = 'Enquiry from Hencework';
+    $mail->AddAddress('xnam7799@gmail.com'); //recipient
+    $mail->Subject = 'Contact From XuanNam_Resume';
     $mail->Body = "Name: " . $_POST['inputName'] . "\r\n\r\nMessage: " . stripslashes($_POST['inputMessage']);
 
     if (isset($_POST['ref'])) {
